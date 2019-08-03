061082fd5d99ac2039e3b79d9c0fa6f52f99f49f
Toby <tobias76@users.noreply.github.com>
2019-08-03 17:49:32 +0100

    Code Additions
    Added a check to not write to text any file without a title or abstract.

--- XMLParser.py
+++ XMLParser.py
@@ -11,11 +11,12 @@ import glob
 import sys
 
 outerDict = {}
+entriesRemoved = {}
 
 class newXMLParser():
     
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-    absPath = os.path.join(desktop, "PVDCorpus19601986")  
+    absPath = os.path.join(desktop, "HematologyCorpus")  
     i = 0    
     def readXML():
 
@@ -52,27 +53,45 @@ class newXMLParser():
                     
                 outerDict[PMIDSaved] = [titleSaved, abstractSaved]
         
-        return outerDict                            
+        return outerDict                                                       
                 
     def saveAbs():
         for PMID, innerDict in outerDict.items():
             currentPMID = PMID
-            
-            titleAbsPairing = outerDict[PMID]
-            
-            title = titleAbsPairing[0] + "\n"
-            abstract = titleAbsPairing[1]
-            
+                   
+            if innerDict[0] is not None:
+                title = innerDict[0] + "\n"
+            else:
+                title = "No Title Found"
+                
+            if innerDict[1] is not None:
+                abstract = innerDict[1]
+            else:
+                abstract = "No Abstract Found"
+                
             fileName = str(currentPMID) + ".txt"
             
-            if abstract != "No Abstract Found":
-                if title != "No Title Found":
-                    with open(newXMLParser.absPath + "\\" + fileName, "w+") as file:
-                        file.write(str(title.encode(sys.stdout.encoding, errors='replace')))
-                        file.write(str(abstract.encode(sys.stdout.encoding, errors='replace')))
-                        file.close()
-                        newXMLParser.i += 1
-                         
+            if abstract == None:
+                    entriesRemoved[str(PMID)] = "Removed due to lack of abstract."
+            if abstract ==  "No Abstract Found":
+                entriesRemoved[str(PMID)] = "Removed due to lack of abstract."
+
+            if title == None:
+                if str(PMID) not in entriesRemoved:
+                    entriesRemoved[str(PMID)] = "Removed due to lack of title."
+            if title ==  "No Title Found":
+                entriesRemoved[str(PMID)] = "Removed due to lack of title."
+                
+
+            if str(PMID) not in entriesRemoved:
+                with open(newXMLParser.absPath + "\\" + fileName, "w+") as file:
+                    file.write(str(title.encode(sys.stdout.encoding, errors='replace')))
+                    file.write(str(abstract.encode(sys.stdout.encoding, errors='replace')))
+                    file.close()
+                    newXMLParser.i += 1
+              
+        print("Removing " + str(len(entriesRemoved)) + " abstracts or " + str(100 * float(len(entriesRemoved) / len(outerDict))) + "%")
+
 if __name__ == '__main__':
     outerDict = newXMLParser.readXML()
     newXMLParser.saveAbs()
